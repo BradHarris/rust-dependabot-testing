a7686b488e71e1687ae7efd998bb22118f27ef90
Brad Harris <brad.harris22@gmail.com>
2022-07-26 13:06:49 -0400

    feat: initial commit with silly use of crates

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+extern crate actix_web;
+extern crate async_std;
+extern crate axum;
+extern crate jsonwebtoken;
+extern crate mongodb;
+extern crate oauth2;
+extern crate redis;
+extern crate serde;
+extern crate serde_json;
+extern crate sqlx;
+extern crate tokio;
+extern crate warp;
+
+fn main() {
+    println!("Hello, world!");
+}
